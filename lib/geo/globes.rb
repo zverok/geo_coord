f5730fc617838bb8452ab79354f319149694c1c7
zverok <zverok.offline@gmail.com>
2016-05-01 15:44:50 +0300

    Renamings of Globe formulae and their variables

--- lib/geo/globes.rb
+++ lib/geo/globes.rb
@@ -1,8 +1,28 @@
 require 'singleton'
 
-# Credits:
-#  Most of the code/algo, as well as tests were initially borrowed from
-#  [Graticule](https://github.com/collectiveidea/graticule)
+# Notes on this file
+#
+# **Credits:**
+#
+# Most of the initial code/algo, as well as tests were initially borrowed from
+# [Graticule](https://github.com/collectiveidea/graticule).
+#
+# Algo descriptions borrowed from
+#
+# * http://www.movable-type.co.uk/scripts/latlong.html (simple)
+# * http://www.movable-type.co.uk/scripts/latlong-vincenty.html (Vincenty)
+#
+# **On naming and code style:**
+#
+# Two main methods (distance/azimuth between two points and endpoint by
+# startpoint and distance/azimuth) are named `inverse` & `direct` due
+# to solving "two main geodetic problems": https://en.wikipedia.org/wiki/Geodesy#Geodetic_problems
+#
+# Code for them is pretty "un-Ruby-ish", trying to preserve original
+# formulae as much as possible (including use of Greek characters and
+# inconsistent naming of some things: "simple" solution of direct problem
+# names distance `d`, while Vincenty formula uses `s`).
+#
 #
 
 module Geo
@@ -12,27 +32,44 @@ module Geo
       include Math
 
       def distance(from, to)
-        distance_azimuth(from, to).first
+        inverse(from.φ, from.λ, to.φ, to.λ).first
       end
 
       def azimuth(from, to)
-        (distance_azimuth(from, to).last / PI * 180 + 360) % 360
+        (inverse(from.φ, from.λ, to.φ, to.λ).last / PI * 180 + 360) % 360
       end
 
-      def distance_azimuth(from, to)
-        # Haversine formula
-        # See TODO
-        d = acos(
-            sin(from.phi) * sin(to.phi) +
-            cos(from.phi) * cos(to.phi) * cos(to.la - from.la)
-        ) * self.class::RADIUS
+      def endpoint(from, distance, azimuth)
+        Coord.from_rad(*direct(from.φ, from.λ, distance, azimuth * PI / 180))
+      end
 
-        y = sin(to.la - from.la) * cos(to.phi)
-        x = cos(from.phi) * sin(to.phi) -
-            sin(from.phi) * cos(to.phi) * cos(to.la - from.la)
+      protected
+
+      def inverse(φ1, λ1, φ2, λ2)
+        # See http://www.movable-type.co.uk/scripts/latlong.html
+        Δφ = φ2 - φ1
+        Δλ = λ2 - λ1
+        a = sin(Δφ/2)**2 + cos(φ1)*cos(φ2) * sin(Δλ/2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1-a))
+        d = r * c
+        
+        y = sin(Δλ) * cos(φ1)
+        x = cos(φ1) * sin(φ2) - sin(φ1) * cos(φ2) * cos(Δλ)
+
         a = atan2(y, x)
 
         [d, a]
+      end
+
+      def direct(φ1, λ1, d, α1)
+        φ2 = asin( sin(φ1)*cos(d/r) +
+                    cos(φ1)*sin(d/r)*cos(α1) )
+        λ2 = λ1 + atan2(sin(α1)*sin(d/r)*cos(φ1), cos(d/r)-sin(φ1)*sin(φ2))
+        [φ2, λ2]
+      end
+
+      def r
+        self.class::RADIUS
       end
     end
 
@@ -46,65 +83,63 @@ module Geo
       VINCENTY_MAX_ITERATIONS = 20
       VINCENTY_TOLERANCE = 1e-12
 
-      def distance_azimuth(from, to)
+      def inverse(φ1, λ1, φ2, λ2)
         # Vincenty formula
         # See http://www.movable-type.co.uk/scripts/latlong-vincenty.html
-        l = to.la - from.la
-        u1 = atan((1-F) * tan(from.phi))
-        u2 = atan((1-F) * tan(to.phi))
+        l = λ2 - λ1
+        u1 = atan((1-F) * tan(φ1))
+        u2 = atan((1-F) * tan(φ2))
         sin_u1 = sin(u1); cos_u1 = cos(u1)
         sin_u2 = sin(u2); cos_u2 = cos(u2)
 
-        la = l # first approximation
-        prev_la, cosSqAlpha, sin_sigma, cos_sigma, sigma, cos2SigmaM, sin_la, cos_la = nil
+        λ = l # first approximation
+        λʹ, cosSqα, sin_σ, cos_σ, σ, cos2σM, sinλ, cosλ = nil
 
         VINCENTY_MAX_ITERATIONS.times do
-          sin_la = sin(la)
-          cos_la = cos(la)
+          sinλ = sin(λ)
+          cosλ = cos(λ)
 
-          sin_sigma = sqrt((cos_u2*sin_la) * (cos_u2*sin_la) +
-            (cos_u1*sin_u2-sin_u1*cos_u2*cos_la) * (cos_u1*sin_u2-sin_u1*cos_u2*cos_la))
+          sin_σ = sqrt((cos_u2*sinλ) * (cos_u2*sinλ) +
+            (cos_u1*sin_u2-sin_u1*cos_u2*cosλ) * (cos_u1*sin_u2-sin_u1*cos_u2*cosλ))
 
-          return [0, 0] if sin_sigma == 0  # co-incident points
+          return [0, 0] if sin_σ == 0  # co-incident points
 
-          cos_sigma = sin_u1*sin_u2 + cos_u1*cos_u2*cos_la
-          sigma = atan2(sin_sigma, cos_sigma)
-          sin_alpha = cos_u1 * cos_u2 * sin_la / sin_sigma
-          cosSqAlpha = 1 - sin_alpha*sin_alpha
-          cos2SigmaM = cos_sigma - 2*sin_u1*sin_u2/cosSqAlpha
-          cos2SigmaM = 0 if cos2SigmaM.nan?  # equatorial line: cosSqAlpha=0 (§6)
+          cos_σ = sin_u1*sin_u2 + cos_u1*cos_u2*cosλ
+          σ = atan2(sin_σ, cos_σ)
+          sinα = cos_u1 * cos_u2 * sinλ / sin_σ
+          cosSqα = 1 - sinα*sinα
+          cos2σM = cos_σ - 2*sin_u1*sin_u2/cosSqα
+          cos2σM = 0 if cos2σM.nan?  # equatorial line: cosSqα=0 (§6)
 
-          c = F/16*cosSqAlpha*(4+F*(4-3*cosSqAlpha))
-          prev_la = la
-          la = l + (1-c) * F * sin_alpha *
-            (sigma + c*sin_sigma*(cos2SigmaM+c*cos_sigma*(-1+2*cos2SigmaM*cos2SigmaM)))
+          c = F/16*cosSqα*(4+F*(4-3*cosSqα))
+          λʹ = λ
+          λ = l + (1-c) * F * sinα *
+            (σ + c*sin_σ*(cos2σM+c*cos_σ*(-1+2*cos2σM*cos2σM)))
 
-          break if prev_la && (la - prev_la).abs < VINCENTY_TOLERANCE
+          break if λʹ && (λ - λʹ).abs < VINCENTY_TOLERANCE
         end
 
         # Formula failed to converge (happens on antipodal points)
         # We'll call Haversine formula instead.
-        return super if (la - prev_la).abs > VINCENTY_TOLERANCE
+        return super if (λ - λʹ).abs > VINCENTY_TOLERANCE
 
-        uSq = cosSqAlpha * (MAJOR_AXIS**2 - MINOR_AXIS**2) / (MINOR_AXIS**2)
+        uSq = cosSqα * (MAJOR_AXIS**2 - MINOR_AXIS**2) / (MINOR_AXIS**2)
         a = 1 + uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)))
         b = uSq/1024 * (256+uSq*(-128+uSq*(74-47*uSq)))
-        delta_sigma = b*sin_sigma*(cos2SigmaM+b/4*(cos_sigma*(-1+2*cos2SigmaM*cos2SigmaM)-
-          b/6*cos2SigmaM*(-3+4*sin_sigma*sin_sigma)*(-3+4*cos2SigmaM*cos2SigmaM)))
+        Δσ = b*sin_σ*(cos2σM+b/4*(cos_σ*(-1+2*cos2σM*cos2σM)-
+          b/6*cos2σM*(-3+4*sin_σ*sin_σ)*(-3+4*cos2σM*cos2σM)))
 
-        s = MINOR_AXIS * a * (sigma-delta_sigma)
-        alpha1 = atan2(cos_u2*sin_la, cos_u1*sin_u2 - sin_u1*cos_u2*cos_la)
-        #alpha2 = atan2(cos_u1*sin_la, -sin_u1*cos_u2+cos_u1*sin_u2*cos_la) / PI * 180
+        s = MINOR_AXIS * a * (σ-Δσ)
+        α1 = atan2(cos_u2*sinλ, cos_u1*sin_u2 - sin_u1*cos_u2*cosλ)
 
-        [s, alpha1]
+        [s, α1]
       end
 
-      def endpoint(from, distance, azimuth)
-        α1 = azimuth / 180 * PI
+      def direct(φ1, λ1, s, α1)
         sinα1 = sin(α1)
         cosα1 = cos(α1)
 
-        tanU1 = (1-F) * tan(from.φ)
+        tanU1 = (1-F) * tan(φ1)
         cosU1 = 1 / sqrt(1 + tanU1**2)
         sinU1 = tanU1 * cosU1
         σ1 = atan2(tanU1, cosα1)
@@ -114,7 +149,7 @@ module Geo
         a = 1 + uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)))
         b = uSq/1024 * (256+uSq*(-128+uSq*(74-47*uSq)))
 
-        σ = distance / (MINOR_AXIS*a)
+        σ = s / (MINOR_AXIS*a)
         σʹ = nil
         
         begin
@@ -124,7 +159,7 @@ module Geo
             Δσ = b*sinσ*(cos2σM+b/4*(cosσ*(-1+2*cos2σM**2)-
                 b/6*cos2σM*(-3+4*sinσ**2)*(-3+4*cos2σM**2)))
             σʹ = σ
-            σ = distance / (MINOR_AXIS*a) + Δσ
+            σ = s / (MINOR_AXIS*a) + Δσ
         end while (σ-σʹ).abs > 1e-12
 
         tmp = sinU1*sinσ - cosU1*cosσ*cosα1
@@ -133,9 +168,10 @@ module Geo
         c = F/16*cosSqα*(4+F*(4-3*cosSqα))
         l = λ - (1-c) * F * sinα *
             (σ + c*sinσ*(cos2σM+c*cosσ*(-1+2*cos2σM*cos2σM)))
-        λ2 = (from.λ+l+3*PI) % (2*PI) - PI # normalise to -180...+180
 
-        Coord.from_rad(φ2, λ2)
+        λ2 = (λ1+l+3*PI) % (2*PI) - PI # normalise to -PI...+PI
+
+        [φ2, λ2]
       end
     end
   end
